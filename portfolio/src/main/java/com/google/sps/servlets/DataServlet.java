db6fd96464281ae349779d94465ae1e3e3521f0e
hajiler <hajiler@me.com>
2020-06-01 17:44:01 -0500

    Overrides doPost function

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -38,4 +38,9 @@ public class DataServlet extends HttpServlet {
     response.setContentType("application/json;");
     response.getWriter().println(json);
   }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+  }
 }
