8d77a20f422aba42c2f9576038e5f104aa6aaeb6
hajiler <hajiler@me.com>
2020-06-02 19:03:33 -0500

    removes line preventing comments from displaying

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -46,7 +46,6 @@ public class DataServlet extends HttpServlet {
     
     response.setContentType("application/json;");
     response.getWriter().println(json);
-    response.sendRedirect("/index.html");
   }
 
   @Override
