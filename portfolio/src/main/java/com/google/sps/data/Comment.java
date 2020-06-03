198f3c71ee2b7f83487fb81daa015ad6330631a6
hajiler <hajiler@me.com>
2020-06-03 21:36:48 +0000

    Changes parameterized constructor to accept correct types, implements static function to parse Entity, perform needed casting, and return new Comment object

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -1,11 +1,16 @@
 package com.google.sps.data;
 
+import com.google.appengine.api.datastore.Entity;
+
 public final class Comment {
   private final String value;
   private final long timeMillis;
 
-  public Comment(Object comment, Object time){
-    this.value = (String) comment;
-    this.timeMillis = (Long) time;
+  public Comment(String comment, long time){
+    this.value = comment;
+    this.timeMillis = time;
+  }
+  static Comment entityToComment(Entity comment) {
+    return new Comment((String) comment.getProperty("value"),(Long) comment.getProperty("timeMillis"));
   }
 }
