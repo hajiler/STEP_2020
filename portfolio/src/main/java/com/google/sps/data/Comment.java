56923a0267718668803a64bd0b2824a2d1be2a71
hajiler <hajiler@me.com>
2020-06-04 19:38:42 +0000

    Adds name property to comment class, static conversion function and constructor.

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -3,15 +3,17 @@ package com.google.sps.data;
 import com.google.appengine.api.datastore.Entity;
 
 public final class Comment {
+  private final String author;
   private final String value;
   private final long timeMillis;
 
-  public Comment(String comment, long time) {
+  public Comment(String name, String comment, long time) {
+    this.author = name;
     this.value = comment;
     this.timeMillis = time;
   }
 
   public static Comment fromEntity(Entity comment) {
-    return new Comment((String) comment.getProperty("value"), (Long) comment.getProperty("timeMillis"));
+    return new Comment((String) comment.getProperty("name"), (String) comment.getProperty("value"), (Long) comment.getProperty("timeMillis"));
   }
 }
