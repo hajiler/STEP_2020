349245759e60693f6e6e9939a253b1e57b8ae8b1
hajiler <hajiler@me.com>
2020-06-05 19:26:55 +0000

    Adds member to store comment's key from datastore to later identify and delete

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -1,19 +1,23 @@
 package com.google.sps.data;
 
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 
 public final class Comment {
   private final String author;
   private final String value;
+  private final String entityKey;
   private final long timeMillis;
 
-  public Comment(String name, String comment, long time) {
+  public Comment(String name, String comment, String key, long time) {
     this.author = name;
     this.value = comment;
+    this.entityKey = key;
     this.timeMillis = time;
   }
 
   public static Comment fromEntity(Entity comment) {
-    return new Comment((String) comment.getProperty("name"), (String) comment.getProperty("value"), (Long) comment.getProperty("timeMillis"));
+    return new Comment((String) comment.getProperty("name"), (String) comment.getProperty("value"), KeyFactory.keyToString(comment.getKey()), (Long) comment.getProperty("timeMillis"));
   }
 }
